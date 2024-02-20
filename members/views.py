3c3229f7045b543dc84a560466cc872216d80cc1
Alberto Tuesta <betock100@yahoo.com>
2024-02-20 17:18:47 -0500

    linkIntegrated

--- members/views.py
+++ members/views.py
@@ -1,13 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Member
 
 # Create your views here.
 def members(request):
     mymembers = Member.objects.all().values()
+    mymember1 = Member.objects.get(id=1)
     template = loader.get_template('all_members.html')
     context = {
         'mymembers' : mymembers,
+        'mymember1' :mymember1,
     }
     return HttpResponse(template.render(context, request))
 
@@ -22,3 +24,11 @@ def details(request, id):
 def main(request): 
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def testing(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('template.html')
+    context = {
+        'mymembers': mymembers, 
+    }
+    return HttpResponse(template.render(context, request))
